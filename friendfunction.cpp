021c1cde9e53f7ee1eb183e6f7659f9baa51972f
psydc3 <psydc3@exmail.nottingham.ac.uk>
2017-05-27 00:02:12 +0100

    Lec09 Friend Function

---
+++ friendfunction.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+
+class Friendly{
+private:
+    int _i;
+public:
+    Friendly(int i = 4): _i(i){
+
+    }
+
+    friend void FriendFunc(const char * msg, Friendly & ob);
+
+};
+
+void FriendFunc(const char * msg, Friendly &ob){
+    printf( "%s : _i = %d\n", msg, ob._i );
+}
+
+int main(){
+    Friendly d1(2), d2;
+    FriendFunc("d1", d1);
+    FriendFunc("d2", d2);
+}
