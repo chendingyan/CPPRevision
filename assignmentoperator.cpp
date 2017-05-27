7f025619a2458bc9e9bf1ba12d5b414afd42b926
psydc3 <psydc3@exmail.nottingham.ac.uk>
2017-05-27 14:50:10 +0100

    Lec13 Assignment Operator

---
+++ assignmentoperator.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+
+class Example{
+public:
+    Example(int val = 1)
+    :m_Val(val){
+    }
+
+    Example &operator= (const Example& rhs){
+        m_Val = rhs.m_Val+10;
+        return *this;
+    }
+
+    void print(){
+        printf("%d\n", m_Val);
+    }
+private:
+    int m_Val;
+};
+
+int main(){
+    Example eg5(4);
+    Example eg6(5);
+    Example eg7(6);
+
+    //Assignment
+    eg7 = eg6 = eg5;
+
+    eg5.print();
+    eg6.print();
+    eg7.print();
+
+
+    return 0;
+
+}
+
