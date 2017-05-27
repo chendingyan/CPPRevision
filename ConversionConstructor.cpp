fa8f0fcfb225724d7539391d2af8e71185328965
psydc3 <psydc3@exmail.nottingham.ac.uk>
2017-05-27 15:09:18 +0100

    Lec13 Conversion Constructor and Conversion Operator

---
+++ ConversionConstructor.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class Converter{
+public:
+    Converter(int i = 4);
+    operator int();
+    int _i;
+};
+
+//conversion constructor
+Converter::Converter(int i):_i(i){
+    cout << "Constructing from int\n";
+};
+
+//conversion operator
+Converter::operator int() {
+    cout <<"Constructing to int\n";
+    return _i;
+}
+
+int main(){
+    int i =14;
+    Converter c1(5);
+    Converter c2 = i;
+    cout << c2._i;
+
+    int j = (int)c1;
+    cout << j;
+}
