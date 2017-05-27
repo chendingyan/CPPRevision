350e1f5c02d9f2d285df54b95a94258b48f2709d
psydc3 <psydc3@exmail.nottingham.ac.uk>
2017-05-27 14:07:55 +0100

    Exam Question about reference and virtual functions Quite Tricky

---
+++ exam question1 reference and virtual functions.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+
+class Base{
+protected:
+    int i;
+public:
+    virtual int a() { return  i+1; }
+    int b(){ return i+2; }
+    virtual void set(int n){i = n;}
+
+};
+
+class Sub: public Base{
+public:
+    int a(){ return i+3; }
+    int b(){ return i+4; }
+    void set(int n){i = n+5;}
+};
+
+int main(){
+    Sub s;
+    Base& r = s;
+    s.set(10);
+    printf("%d, %d, %d, %d\n", s.a(), s.b(), r.a(), r.b());
+
+    r.set(20);
+    printf("%d, %d, %d, %d\n", s.a(), s.b(), r.a(), r.b());
+    return 0;
+
+}
